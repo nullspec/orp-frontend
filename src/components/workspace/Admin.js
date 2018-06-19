71fef237142800ddd37b5000f64e04f1856f97e5
Nick Pate <saleen34@gmail.com>
2018-06-18 21:48:18 -0700

    Saving work

---
+++ src/components/workspace/Admin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class AdminWorkspace extends Component {
+  render() {
+    return (
+      <div className="AdminWorkspace">
+      Admin
+      </div>
+    );
+  }
+}
+
+export default AdminWorkspace;
---
+++ src/components/workspace/Member.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MemberWorkspace extends Component {
+  render() {
+    return (
+      <div className="MemberWorkspace">
+      Member
+      </div>
+    );
+  }
+}
+
+export default MemberWorkspace;
